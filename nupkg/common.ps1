4f777c485765115a06b0a4a679fb69927ce09490
fjq <180489097@qq.com>
2021-04-01 16:38:00 +0800

    111

--- nupkg/common.ps1
+++ nupkg/common.ps1
@@ -12,12 +12,12 @@ $solutions = (
 # List of projects
 $projects = (
     # Shared
-    "Laison.Lapis.Shared/src/Laison.Lapis.Application.Shared",
-    "Laison.Lapis.Shared/src/Laison.Lapis.Domain.Shared",
-    "Laison.Lapis.Shared/src/Laison.Lapis.EntityFrameworkCore.Shared",
-    "Laison.Lapis.Shared/src/Laison.Lapis.Host.Shared",
-    "Laison.Lapis.Shared/src/Laison.Lapis.HttpApi.Shared",
     "Laison.Lapis.Shared/src/Laison.Lapis.Shared",
+    "Laison.Lapis.Shared/src/Laison.Lapis.Shared.Application",
+    "Laison.Lapis.Shared/src/Laison.Lapis.Shared.Domain",
+    "Laison.Lapis.Shared/src/Laison.Lapis.Shared.EntityFrameworkCore",
+    "Laison.Lapis.Shared/src/Laison.Lapis.Shared.Host",
+    "Laison.Lapis.Shared/src/Laison.Lapis.Shared.HttpApi",
     # Identity
     "Laison.Lapis.Identity/src/Laison.Lapis.Identity.Application",
     "Laison.Lapis.Identity/src/Laison.Lapis.Identity.Application.Contracts",
