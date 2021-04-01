2d2d40c4778f286d5c7203af16fb9be52f2f1c6a
fjq <180489097@qq.com>
2021-04-01 14:07:54 +0800

    update

---
+++ Laison.Lapis.Identity/src/Laison.Lapis.Identity.HttpApi/User/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Laison.Lapis.Identity.Application.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace Laison.Lapis.Identity.HttpApi
+{
+    [RemoteService]
+    [Route("api/Identity/user")]
+    public class UserController : IdentityController, IUserAppService
+    {
+        private readonly IUserAppService _userAppService;
+
+        public UserController(IUserAppService orderAppService)
+        {
+            _userAppService = orderAppService;
+        }
+
+        [HttpGet]
+        public Task<UserDto> GetUserDetailsAsync([FromQuery] Guid id)
+        {
+            return _userAppService.GetUserDetailsAsync(id);
+        }
+
+        [HttpPost]
+        public Task CreateUserAsync(CreateUserInput input)
+        {
+            return _userAppService.CreateUserAsync(input);
+        }
+    }
+}
