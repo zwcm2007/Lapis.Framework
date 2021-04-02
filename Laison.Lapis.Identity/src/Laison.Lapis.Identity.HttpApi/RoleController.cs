16dc3338e200a46bdee07205043c1149eb15632b
fjq <180489097@qq.com>
2021-04-02 16:36:58 +0800

    update

---
+++ Laison.Lapis.Identity/src/Laison.Lapis.Identity.HttpApi/RoleController.cs
@@ -0,0 +1,29 @@
+﻿using Laison.Lapis.Identity.Application.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Laison.Lapis.Identity.HttpApi
+{
+    //[RemoteService(Name = IdentityRemoteServiceConsts.RemoteServiceName)]
+    //[Area("identity")]
+    //[ControllerName("Role")]
+    [Route("api/identity/roles")]
+    public class RoleController : AbpController, IRoleAppService
+    {
+        private readonly IRoleAppService _roleAppService;
+
+        public RoleController(IRoleAppService roleAppService)
+        {
+            _roleAppService = roleAppService;
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public virtual Task<ListResultDto<RoleDto>> GetAllListAsync()
+        {
+            return _roleAppService.GetAllListAsync();
+        }
+    }
+}
