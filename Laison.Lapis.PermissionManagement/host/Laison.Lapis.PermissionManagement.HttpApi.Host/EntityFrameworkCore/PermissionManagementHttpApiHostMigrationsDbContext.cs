87c4f5c2fa840309082313dec79f4d5ac57ad9a2
fjq <180489097@qq.com>
2021-04-15 05:10:31 +0800

    update

---
+++ Laison.Lapis.PermissionManagement/host/Laison.Lapis.PermissionManagement.HttpApi.Host/EntityFrameworkCore/PermissionManagementHttpApiHostMigrationsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace Laison.Lapis.PermissionManagement.EntityFrameworkCore
+{
+    public class PermissionManagementHttpApiHostMigrationsDbContext : AbpDbContext<PermissionManagementHttpApiHostMigrationsDbContext>
+    {
+        public PermissionManagementHttpApiHostMigrationsDbContext(DbContextOptions<PermissionManagementHttpApiHostMigrationsDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ConfigurePermissionManagement();
+        }
+    }
+}
---
+++ Laison.Lapis.PermissionManagement/host/Laison.Lapis.PermissionManagement.HttpApi.Host/EntityFrameworkCore/PermissionManagementHttpApiHostMigrationsDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Laison.Lapis.PermissionManagement.EntityFrameworkCore
+{
+    public class PermissionManagementHttpApiHostMigrationsDbContextFactory : IDesignTimeDbContextFactory<PermissionManagementHttpApiHostMigrationsDbContext>
+    {
+        public PermissionManagementHttpApiHostMigrationsDbContext CreateDbContext(string[] args)
+        {
+            var configuration = BuildConfiguration();
+
+            var connStr = configuration.GetConnectionString("PermissionManagement");
+            var builder = new DbContextOptionsBuilder<PermissionManagementHttpApiHostMigrationsDbContext>()
+                .UseMySql(connStr, ServerVersion.AutoDetect(connStr));
+
+            return new PermissionManagementHttpApiHostMigrationsDbContext(builder.Options);
+        }
+
+        private static IConfigurationRoot BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+
+            return builder.Build();
+        }
+    }
+}
